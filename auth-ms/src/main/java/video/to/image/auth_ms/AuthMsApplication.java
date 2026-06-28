7b210e1991138394420812ba6e352fe8ce134cce
caiqueluci0 <usuario@DESKTOP-EVFGSQK.localdomain>
2026-06-28 01:58:16 -0300

    spring initializer

---
+++ auth-ms/src/main/java/video/to/image/auth_ms/AuthMsApplication.java
@@ -0,0 +1,13 @@
+package video.to.image.auth_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthMsApplication.class, args);
+	}
+
+}
---
+++ auth-ms/src/test/java/video/to/image/auth_ms/AuthMsApplicationTests.java
@@ -0,0 +1,13 @@
+package video.to.image.auth_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
