b51bd8cccd2f956bab1614fdf1fafe2a5cada5a2
caiqueluci0 <usuario@DESKTOP-EVFGSQK.localdomain>
2026-06-29 19:57:03 -0300

    spring initializer

---
+++ management_ms/src/main/java/video/to/image/management_ms/ManagementMsApplication.java
@@ -0,0 +1,13 @@
+package video.to.image.management_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementMsApplication.class, args);
+	}
+
+}
---
+++ management_ms/src/test/java/video/to/image/management_ms/ManagementMsApplicationTests.java
@@ -0,0 +1,13 @@
+package video.to.image.management_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
